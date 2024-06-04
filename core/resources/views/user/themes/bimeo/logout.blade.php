e6327993802f522dc6b9d4e561fcd7931e6ca9f5
ijidev <iheanyichukwui94@gmail.com>
2024-06-04 15:32:43 +0100

    updated logout spelling error

--- core/resources/views/user/themes/bimeo/logout.blade.php
+++ core/resources/views/user/themes/bimeo/logout.blade.php
@@ -5,7 +5,7 @@
     <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
         <div class="modal-content border-top border-bottom" style=" height:auto; margin: 0 auto; box-shadow:4px 4px 5px 3px #999;">
             <div class="modal-header border-0 bg-template home-to-cs-text" style="color: black; text-align:center;">
-                <h5>Are u sure u want to logout?</h5>
+                <h5>Are you sure you want to logout?</h5>
             </div>
             <div class="modal-footer border-0 my-browser-flex my-browser-flex-center my-browser-flex-space-between" style="width: 100%;">
                 <button style="margin:20px; padding:5px;" class="btn btn-default btn-sm btn-rounded shadow " onclick="window.location.href='{{route('logout')}}'">Confirm</button>
